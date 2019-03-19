61923a6376f9363089123b7050435f4a304e87e7
mchanni1321 <Mehak.Channi76@qmail.cuny.edu>
2019-03-18 22:48:54 -0400

    Updated Twitter Client to compose tweets

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -2,7 +2,9 @@ package com.codepath.apps.restclienttemplate.models;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
+@Parcel
 public class User {
 
     public String name;
@@ -10,6 +12,10 @@ public class User {
     public String screenName;
     public String profileImageUrl;
 
+
+    public User() {
+
+    }
 
     public static User fromJson(JSONObject jsonObject) throws JSONException {
 
